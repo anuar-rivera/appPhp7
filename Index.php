02c00d1f00f018e37de5d284b86e618261f23faf
anuar-rivera <58532968+anuar-rivera@users.noreply.github.com>
2019-12-11 13:30:06 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -11,23 +11,7 @@
     <link rel="stylesheet" href="css/problema.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-    <script>
-        function solucion1(){
-        var densidad = 700;
-        var volumen = 14500;
-        var gravedad = 9.8; 
-        
 
-        var conversion= volumen/1000; 
-        var masa = densidad*conversion; 
-        var peso = masa*gravedad; 
-
-        var d=document.getElementById("resultado1");
-        d.innerHTML="V ="+conversion+" m^3"+"<br>"+"M ="+masa+" Kg"+"<br>"+"W ="+peso+" N";
-
-        
-        }
-    </script>
 </head>
 <body>
     <section id="contenedor">
@@ -63,7 +47,10 @@
             </p>
       </section>
       <section id="solucion">
-            <center><h2>Calculos</h2></center>
+
+            
+        
+            <center><h2>Cálculos</h2></center>
             <p> V = 14500/1000 <br> 
                 M = 700*14.5 <br> 
                 W = 10150*9.8 
@@ -71,10 +58,20 @@
                  
       </section>
       <section id="resultado">
-          <center><h2>Solusion</h2></center>
-        <center><button class="boton" onclick="solucion1()">Presiona para calcular</button></center>
+          <center><h2>Solución</h2></center>
+        
         <div class=solution id="resultado1">
-
+          <?php 
+          $densidad = 700;
+          $volumen = 14500;
+          $gravedad = 9.8;
+          $conversion= $volumen/1000; 
+          $masa = $densidad*$conversion; 
+          $peso = $masa*$gravedad; 
+          
+          
+          print "Masa = " .$masa."<br> Peso =".$peso."<br> Volumen =".$conversion ;
+          ?>
         </div><br>
       </section>
     </section>
